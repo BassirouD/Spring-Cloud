9122d9b29a6d299c1e93ed0cd612b5f1be6dd0e2
Bassirou Diallo <bassiroutdsi@gmail.com>
2021-04-16 12:36:18 +0000

    Ajout de hystrix dashbord et quelque modif

--- gateway-service/src/main/java/org/sid/GatewayServiceApplication.java
+++ gateway-service/src/main/java/org/sid/GatewayServiceApplication.java
@@ -8,21 +8,44 @@ import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitio
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
+@EnableHystrix
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
-    //@Bean
+    @Bean
     RouteLocator staticRoutes(RouteLocatorBuilder builder){
         return builder.routes()
                 .route(r->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
                 .route(r->r.path("/products/**").uri("lb://INVENTORY-SERVICE"))
-                .route(r->r.path("/bills/**").uri("http://localhost:8083/"))
+                .route(r->r
+                        .path("/publicCountries/**")
+                        .filters(f->f
+                                .addRequestHeader("x-rapidapi-host", "restcountries-v1.p.rapidapi.com")
+                                .addRequestHeader("x-rapidapi-key", "7c5cd2bc01mshfde02a7a6027b61p1669d1jsnadc6aa04ff2e")
+                                .rewritePath("/publicCountries/(?<segment>.*)", "/${segment}")
+//                                .hystrix(h->h.setName("countries").setFallbackUri(":/defaultCountries"))
+                            )
+                        .uri("https://restcountries-v1.p.rapidapi.com/"))
+                .route(r->r
+                        .path("/muslim/**")
+                        .filters(f->f
+                                .addRequestHeader("x-rapidapi-host", "muslimsalat.p.rapidapi.com")
+                                .addRequestHeader("x-rapidapi-key", "7c5cd2bc01mshfde02a7a6027b61p1669d1jsnadc6aa04ff2e")
+                                .rewritePath("/muslim/(?<segment>.*)", "/${segment}")
+                        )
+                        .uri("https://muslimsalat.p.rapidapi.com/"))
                 .build();
       }
 
@@ -32,4 +55,16 @@ public class GatewayServiceApplication {
 
         return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
       }
+}
+
+//Pour Hystrix mais problem
+@RestController
+class CircuitBreakerRestController{
+    @GetMapping("/defaultCountries")
+    public Map<String, String> countries(){
+        Map<String, String> data = new HashMap<>();
+        data.put("message", "default Countries");
+        data.put("countries", "Senegal, Guinee, Mali, ....");
+        return data;
+    }
 }
